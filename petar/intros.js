27007bef397bae6d72582902d1c7bd899dc6c51c
Petar Vukmanovic <petar.vukmanovic@pannonians.com>
2022-07-22 15:48:08 +0200

    neka poruka, stringovi

---
+++ petar/intros.js
@@ -0,0 +1,31 @@
+console.log("1: " +  typeof true); 
+console.log("2: " +  typeof Boolean(true)); 
+console.log("3: " +  typeof new Boolean(true)); 
+console.log("4: " +  typeof new Boolean(true).valueOf()); 
+console.log("5: " + typeof "abc"); 
+console.log("6: " + typeof String("abc")); 
+console.log("7: " + typeof new String("abc")); 
+console.log("8: " + typeof new String("abc").valueOf()); 
+console.log("9: " +  typeof 123); 
+console.log("10: " + typeof Number(123)); 
+console.log("11: " + typeof new Number(123));
+console.log("11: " + typeof new Number(123).valueOf());
+
+// var broj = 11;
+// const string = "11";
+// let boolean = false;
+
+// // console.log(typeof broj);
+// // console.log(typeof string);
+// // console.log(typeof boolean);
+
+// // console.log(typeof Number())
+// // console.log(typeof new String('qwe').valueOf())
+// // console.log(new String('qwe').valueOf())
+// // console.log(Boolean())
+
+// var nekiAndefined;
+// var nista = null;
+
+// console.log(typeof nekiAndefined)
+// console.log(typeof nista)
