04e982a699ca4b827c081740a890b389fe7d1359
David Mandic <david.mandic@pannonians.com>
2022-07-26 10:54:50 +0200

    Solving index.js conflict issue

--- david/index.js
+++ david/index.js
@@ -2,25 +2,3 @@ var string = 'David';
 var number = 34
 
 console.log('My name is ' + string + '. I am ' + number + '!' );
-
-<<<<<<< HEAD
-//console.log(found);
-//console.log(`This is repeate text: ${text.repeat(5)}`);
-//console.log(text.replace('fox', 'car'));
-//console.log(text.replaceAll('dox', 'car'));
-//console.log(text.search('fox'));
-//console.log(text.slice(0, 9));
-//console.log(text.startsWith('dog'));
-//console.log(text.substring(0, 2));
-//console.log(text.trim());
-
-const stringObj = new String('David');
-
-<<<<<<< HEAD
-//console.log(stringObj);
-=======
-//console.log(stringObj);
-//done
->>>>>>> b0947bd4d8c8fdf0ffece2fa75255a3a52a43277
-=======
->>>>>>> 51c0164dfb4ef79f49e9ba904ddb29fde0aa5d8c
