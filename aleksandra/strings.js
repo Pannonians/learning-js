0bc883d49eee37470563c710e13049a1399e9e5e
Aleksandra Vukasinovic <109579063+NanaAV@users.noreply.github.com>
2022-07-27 10:08:14 +0200

    Nana/hw (#17)
    * test
    
    * funkcije na stringovima

---
+++ aleksandra/strings.js
@@ -0,0 +1,40 @@
+var string = "neki string ";
+var noviString = "neki novi string";
+const str = "STR";
+
+//console.log(string + ", a i " + noviString);
+//console.log(string+=noviString);
+
+//console.log(string.concat(' ', noviString));
+//console.log(string.endsWith('ng'));
+//console.log(`Slog "${str}" ${string.includes(str) ? 'jeste' : 'nije'} u stringu`  );
+//console.log(`Index prvog pojavljivanja "${str}" od pocetka je ${string.indexOf(str)}`);
+//console.log(`Index prvog pojavljivanja "${str}" sa kraja je ${string.lastIndexOf(str)}`);
+
+const regex = /[A-Z]/g;
+
+//console.log(noviString.match(regex));
+
+//console.log(string.padEnd(20, '*'));
+//console.log(string.slice(-7).padStart(string.length, '*'));
+//console.log(`Putanja fajla je: ${String.raw`C:/development/learning-js/aleksandra/strings.js`}`);
+//console.log(`Ucitavanje stringa: ${string.repeat(3)} `);
+//console.log(noviString.replace('n', 'N'));
+//console.log(noviString.slice(5));
+//console.log(noviString.slice(4, 5));
+//console.log(noviString.split(' '));
+//console.log(noviString.split(' ', 2));
+//console.log(string.startsWith('ne'));
+//console.log(string.startsWith('ne', 4));
+//console.log(string.substring(4, 8));
+//console.log(str.toLocaleLowerCase());
+//console.log(string.toLocaleUpperCase());
+//console.log(string.trim());
+//console.log(string.trimStart());
+
+console.log(`To bi bilo sve za sada, dok nam ne zatreba ${noviString} ;) .`);
+
+
+
+
+
