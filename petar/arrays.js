f63510e4391759f81b5e5e39ca5d1920c4a83cdc
pvukmanovic-pannonians <90039609+pvukmanovic-pannonians@users.noreply.github.com>
2022-07-28 15:23:36 +0200

    reducer explained (#29)

--- petar/arrays.js
+++ petar/arrays.js
@@ -1,3 +1,7 @@
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
 const createPerson = (firstName, lastName) => {
   const fullName = `${firstName} ${lastName}`;
 
@@ -6,8 +10,19 @@ const createPerson = (firstName, lastName) => {
     lastName,
     fullName,
     activated: true,
+    title: jobTitles.random(),
   };
 };
+
+const jobTitles = [
+  "Software Engineer",
+  "Q/A Engineer",
+  "Data Engineer",
+  "DevOps Engineer",
+  "Scrum Master",
+  "Product Owner",
+  "Human Resources",
+];
 
 function run() {
   console.log("We are currently running app called arrays");
@@ -17,38 +32,86 @@ function run() {
   const array = [
     createPerson("Nikolina", "Novakovic"),
     createPerson("David", "Mandic"),
+    createPerson("Petar", "Vukmanovic"),
+    createPerson("Mirko", "Vasic"),
+    createPerson("Stefan", "Dankuc"),
+    createPerson("Dusan", "Gajic"),
+    createPerson("Milan", "Tesic"),
+    createPerson("Igor", "Popovic"),
+    createPerson("Tijana", "Golovic"),
     aleksandra,
   ];
 
-  const newArray = [...array, createPerson("Petar", "Vukmanovic")];
+  console.log(jobTitles.random());
 
-  console.log("duzina niza", newArray.length);
-  console.log("newArray", newArray);
+  // console.log(
+  //   "banalan primer",
+  //   array.reduce((acc, covek) => {
+  //     acc.push(covek.fullName.toUpperCase());
+  //     return acc;
+  //   }, [])
+  // );
+
+  // console.log("array", array);
+
+  // console.log(
+  //   "reduced po zaposlenju: ",
+  //   array.reduce((acc, user) => {
+  //     if (!acc[user.title]) {
+  //       acc[user.title] = [];
+  //     }
+  //     acc[user.title].push(user);
+  //     return acc;
+  //   }, {})
+  // );
+
+  // console.log(
+  //   "po alfabeti",
+  //   array.reduce((acc, user) => {
+      
+  //     if (!acc[user.firstName[0]]) {
+  //       acc[user.firstName[0]] = [];
+  //     }
+
+  //     acc[user.firstName[0]].push(user);
+  //     // return acc;
+
+  //     return Object.keys(acc)
+  //       .sort()
+  //       .reduce((obj, key) => {
+  //         obj[key] = acc[key];
+  //         return obj;
+  //       }, {});
+  //   }, {})
+  // );
+
+  // console.log("duzina niza", newArray.length);
+  // console.log("newArray", newArray);
 
   // hocu da u svaki od nizova stavim inicijale
-  const inicijali = newArray.map((jedanOdNiza) => {
-    // const initials = jedanOdNiza.firstName[0] + jedanOdNiza.lastName[0];
-    const { firstName, lastName } = jedanOdNiza;
+  // const inicijali = newArray.map((jedanOdNiza) => {
+  //   // const initials = jedanOdNiza.firstName[0] + jedanOdNiza.lastName[0];
+  //   const { firstName, lastName } = jedanOdNiza;
 
-    return {
-      ...jedanOdNiza,
-      initials: firstName[0] + lastName[0],
-    };
-  });
-  console.log("mapirano da ima inicijale", inicijali);
+  //   return {
+  //     ...jedanOdNiza,
+  //     initials: firstName[0] + lastName[0],
+  //   };
+  // });
+  // console.log("mapirano da ima inicijale", inicijali);
 
-  console.log(
-    "filtrirano",
-    inicijali.filter((jedanUNizu) => ["AV", "NN"].includes(jedanUNizu.initials))
-  );
+  // console.log(
+  //   "filtrirano",
+  //   inicijali.filter((jedanUNizu) => ["AV", "NN"].includes(jedanUNizu.initials))
+  // );
 
-  console.log(
-    "nadji Davida",
-    Object.assign(
-      {},
-      inicijali.find((nekaVarijabla) => nekaVarijabla.firstName === "David")
-    )
-  );
+  // console.log(
+  //   "nadji Davida",
+  //   Object.assign(
+  //     {},
+  //     inicijali.find((nekaVarijabla) => nekaVarijabla.firstName === "David")
+  //   )
+  // );
 }
 
 module.exports = {
