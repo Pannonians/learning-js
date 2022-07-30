4d33141c3ab3c9d42e000ff751072d70efbe31a9
David Mandic <david.mandic@pannonians.com>
2022-07-30 17:31:02 +0200

    Example of reliable cars - Exercise

--- david/index.js
+++ david/index.js
@@ -1,8 +1,9 @@
 //CommonJS module let you encapsulate all sorts of functionality, and expose this functionality to other JavaScript files, as libraries
 //The syntax to import a module is:
-const app = require("./array");
+//const app = require("./array");
 //const app = require("./todo");
 //const app = require("./arraysreduce");
 //const app = require("./strings");
+const app = require("./reliableCars");
 
 app.run();
