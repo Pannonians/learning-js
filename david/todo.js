0f0d6b454b6b8702e25959d5c1ca41b5ed6e193d
pvukmanovic-pannonians <90039609+pvukmanovic-pannonians@users.noreply.github.com>
2022-08-02 11:56:13 +0200

    yes (#42)

--- david/todo.js
+++ david/todo.js
@@ -13,6 +13,7 @@ function run(){
     console.log(listica);
 
     todolist = listica.map((todoListItem, index) => {
+
         if (index === 2 || index === 3) {
             todoListItem.toggle();
             return todoListItem;
@@ -23,9 +24,8 @@ function run(){
     })
 
     console.log("Switch 3rd and 4th todo to be completed", listica);
-
     console.log("Print only todos that are completed", listica.filter((item) => item.complited));
-    console.log("Print only todos that are not completed", listica.filter((item) => !item.complited));
+    console.log("Print only todos that are not completed", listica.filter((item) => !item.complited));  
 }
 
 module.exports = {
