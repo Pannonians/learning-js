c16deffc1525e30b80d41bc55365db3c1f3bc0c6
DavidMandic <90040095+PannoniansDavid@users.noreply.github.com>
2022-08-15 15:11:31 +0200

    Davidmandic/project/repo explorer (#47)
    * Configuration of Repo Explorer workspace
    
    * Yarn add chalk to repoexplorer
    
    * RepoExplorer homework without chalk

--- david/index.js
+++ david/index.js
@@ -1,11 +1,11 @@
 //CommonJS module let you encapsulate all sorts of functionality, and expose this functionality to other JavaScript files, as libraries
 //The syntax to import a module is:
-//const app = require("./array");
+const app = require("./array");
 //const app = require("./todo");
 //const app = require("./arraysreduce");
 //const app = require("./strings");
 //const app = require("./reliableCars");
-const app = require("./todo");
+//const app = require("./todo");
 
 
 
