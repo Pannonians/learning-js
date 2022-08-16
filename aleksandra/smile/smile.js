dae13221de7068fc72f2244d45a1677bc1199a5d
Aleksandra Vukasinovic <azobenica@gmail.com>
2022-08-16 11:26:54 +0200

    imported axios and fetch the data from given endpoint

--- aleksandra/smile/smile.js
+++ aleksandra/smile/smile.js
@@ -1,5 +1,21 @@
+const axios = require('axios').default;
+
 function run() {
     console.log("Smile!");
+
+    axios.get('https://gist.githubusercontent.com/VPetar/e1cd462371d438ff863e09983072007b/raw/52087f314f4f763ae0a81fcb9f340bf0a1f2a41a/json')
+        .then(function (response) {
+    // handle success
+         console.log(response);
+        })
+        .catch(function (error) {
+    // handle error
+         console.log(error);
+        })
+        .then(function () {
+    // always executed
+        });
+
 }
   
 module.exports = { run };
