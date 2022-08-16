c83dda7b3c89fcdb04811ccb27db59165f109ab2
Aleksandra Vukasinovic <azobenica@gmail.com>
2022-08-16 17:16:25 +0200

    homework without last step

--- aleksandra/smile/smile.js
+++ aleksandra/smile/smile.js
@@ -1,20 +1,23 @@
 const axios = require('axios').default;
+const process = require('process');
 
-function run() {
+async function smiles() {
+    try {
+        const {data} = await axios.get ('https://gist.githubusercontent.com/VPetar/e1cd462371d438ff863e09983072007b/raw/52087f314f4f763ae0a81fcb9f340bf0a1f2a41a/json');
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+async function run() {
     console.log("Smile!");
 
-    axios.get('https://gist.githubusercontent.com/VPetar/e1cd462371d438ff863e09983072007b/raw/52087f314f4f763ae0a81fcb9f340bf0a1f2a41a/json')
-        .then(function (response) {
-    // handle success
-         console.log(response);
-        })
-        .catch(function (error) {
-    // handle error
-         console.log(error);
-        })
-        .then(function () {
-    // always executed
-        });
+    const [ , , ...args] = process.argv;
+    console.log(args);
+
+    const allSmiles = await smiles();
+    console.log(allSmiles);
 
 }
   
