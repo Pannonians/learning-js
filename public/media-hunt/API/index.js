c0dc8718efe57d12137240672aea95ab2501d246
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-08-24 14:17:40 +0200

    54 discover file removed from index.js

--- public/media-hunt/API/index.js
+++ public/media-hunt/API/index.js
@@ -1,12 +1,10 @@
 import configuration from './configuration';
-import discover from './discover';
 import movie from './movie';
 import tv from './tv';
 import tvSeason from './tvSeason';
 
 export {
     configuration,
-    discover,
     movie,
     tv,
     tvSeason
