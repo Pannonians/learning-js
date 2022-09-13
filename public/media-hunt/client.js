2c314f544c0830506ecb97bb81743469e08a48a7
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-09-13 09:37:08 +0200

    API client using axios created properly with header

--- public/media-hunt/client.js
+++ public/media-hunt/client.js
@@ -1,8 +1,11 @@
 const axios = require("axios");
+
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   timeout: 2500,
-  params: {api_key: '109f8c8c06f943d5e337e7141b1dad67'}
 });
 
-module.exports = {instance};
+instance.default.headers.common = {
+  authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzEwZDg2NTM4OTkzYmNhMjNlNWUyOWNkYjk3NGFlNCIsInN1YiI6IjVjMWM5OTQwOTI1MTQxMjdmODRlMWVmNSIsInNjb3BlcyI6WyJh"
+};
+module.exports = { instance };
