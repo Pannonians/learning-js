28279ba72204a3b3c6146b187784befcfc25a69b
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-09-13 18:37:58 +0200

    66 dotenv dependancie added and token placed in env file

--- public/media-hunt/client.js
+++ public/media-hunt/client.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
+require('dotenv').config();
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   timeout: 2500,
 });
 
-instance.default.headers.common = {
-  authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzEwZDg2NTM4OTkzYmNhMjNlNWUyOWNkYjk3NGFlNCIsInN1YiI6IjVjMWM5OTQwOTI1MTQxMjdmODRlMWVmNSIsInNjb3BlcyI6WyJh",
+instance.defaults.headers.common = {
+  Authorization:
+    `Bearer ${process.env.TMDB_API_TOKEN}`
 };
+
 module.exports = { instance };
