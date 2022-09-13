691d59ae6cbd82b85628a0dd605be8dcf93a8b13
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-09-13 09:48:49 +0200

    66 export default replaced with module export

--- public/media-hunt/client.js
+++ public/media-hunt/client.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
@@ -7,8 +7,7 @@ const instance = axios.create({
 });
 
 instance.defaults.headers.common = {
-  Authorization:
-    `Bearer ${process.env.TMDB_API_TOKEN}`
+  Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
 };
 
 module.exports = { instance };
