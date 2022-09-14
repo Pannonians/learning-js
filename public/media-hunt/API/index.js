56310803b0264974dbb27b00e270a26e7acb23f4
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-09-14 09:41:32 +0200

    module exports changes implemented

--- public/media-hunt/API/index.js
+++ public/media-hunt/API/index.js
@@ -1,5 +1,5 @@
-const { configuration } = require("./configuration");
-const { movie } = require("./movie");
-const { tv } = require("./tv");
+const configuration = require("./configuration");
+const movie = require("./movie");
+const tv = require("./tv");
 
-module.export = { configuration, movie, tv };
+module.exports = { configuration, movie, tv };
--- public/media-hunt/API/movie.js
+++ public/media-hunt/API/movie.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   get: {
     discover: {
       url: "/discover/movie",
