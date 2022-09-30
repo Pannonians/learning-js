ff8f6bd60a3c1b0c8cc4f634ec95636db7756f9c
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-09-30 14:20:11 +0200

    #62 requested changes applied plus chalk

--- public/media-hunt/commands/tv/tv.js
+++ public/media-hunt/commands/tv/tv.js
@@ -1,14 +1,18 @@
 const { normalization } = require("../../general-data/normalizeddata");
-const { printStuffToScreen, titleResponse, printImage } = require("../../general-data/printstuftoscreen");
+const {
+  printStuffToScreen,
+  titleResponse,
+  printImage,
+} = require("../../general-data/printstuftoscreen");
 const { getTv } = require("../../http/tv");
 
 const run = async (id) => {
-    const data = await getTv(id);
-    const normalized = await normalization(data);
-    const printStuff = await printStuffToScreen(normalized);
-    const printTitle = await titleResponse(normalized);
-    await printImage(printStuff);
-    console.log(printTitle, printStuff);
-  };
+  const data = await getTv(id);
+  const normalized = await normalization(data);
+  const printStuff = await printStuffToScreen(normalized);
+  const printTitle = await titleResponse(normalized);
+  await printImage(printStuff);
+  console.log(printTitle, printStuff);
+};
 
 module.exports = { run };
--- public/media-hunt/general-data/printstuftoscreen.js
+++ public/media-hunt/general-data/printstuftoscreen.js
@@ -1,40 +1,56 @@
 const imageToAscii = require("image-to-ascii");
 const figlet = require("figlet");
 const { getConfig } = require("../http/configuration");
+const chalk = require ("chalk")
 
 const printStuffToScreen = async (data) => ({
   year: data.year,
   description: data.description,
   review: data.review,
-  cast: data.cast,
+  cast: JSON.stringify(data.cast).split(","),
   image: (await buildPosterUrl()) + data.image,
 });
 
+async function printImage(data) {
+  let promise = new Promise((resolve, reject) => {
+    imageToAscii(
+      data.image,
+      {
+        colored: true,
+        size: {
+          width: 50,
+        },
+      },
+      (err, converted) => {
+        console.log(err || converted);
+        resolve();
+      }
+    );
+  }).catch((err) => {
+    throw err;
+  });
+
+  return promise;
+}
+
 async function titleResponse(data) {
   const newTitle = await figlet.textSync(data.title);
-  return newTitle;
+  return chalk.bgBlackBright.red.bold(newTitle);
 }
 const dataConfig = async () => await getConfig();
 
 const buildPosterUrl = async () => {
   const resp = await dataConfig();
-  const posterPath = resp.images.base_url + resp.images.poster_sizes[4];
+  const posterSize = {
+    w92: resp.images.poster_sizes[0],
+    w154: resp.images.poster_sizes[1],
+    w185: resp.images.poster_sizes[2],
+    w300: resp.images.poster_sizes[3],
+    w500: resp.images.poster_sizes[4],
+    original: resp.images.poster_sizes[5],
+  };
+  const posterPath = resp.images.base_url + posterSize.w500;
   return posterPath;
 };
-
-async function printImage(data) {
-  imageToAscii(
-    data.image,
-    {
-      colored: true,
-      size: {
-        width: 30,
-      },
-    },
-    (err, converted) => {
-      console.log(err || converted);
-    }
-  );
-}
 
 module.exports = { printStuffToScreen, titleResponse, printImage };
