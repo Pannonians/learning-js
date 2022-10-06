d5e375fdf59b6d8831b2ece2e3d285f06a7d11f8
DavidMandic <90040095+PannoniansDavid@users.noreply.github.com>
2022-10-06 14:42:38 +0200

    ReactJS hello world app (#83)
    * ReactJS hello world app
    
    * ReactJS clock

---
+++ david/reactclock/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const Clock = ({date}) => {
+  return <div>{date.toUTCString()}</div>;
+};
+
+function App() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Clock date={date} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
