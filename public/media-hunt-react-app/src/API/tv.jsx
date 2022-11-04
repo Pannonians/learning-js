479e837d9ad37384e1c80d1ed7ef8f6b59386e15
Nikolina Novakovic <109579817+Ninolina88@users.noreply.github.com>
2022-11-04 09:41:55 +0100

    54/nikolina/api schema file (#95)
    * 55/movie-hunt-react-app folder and app created with axios dependencie
    
    * 54/api folder with schemas created

---
+++ public/media-hunt-react-app/src/API/tv.jsx
@@ -0,0 +1,27 @@
+const tv = {
+  get: {
+    discover: {
+      url: "/discover/tv",
+    },
+    tv: {
+      url: (id) => `/tv/${id}`,
+    },
+    genre: {
+      url: "/genre/tv/list",
+    },
+    topRated: {
+      url: "/tv/top_rated",
+    },
+    reviews: {
+      url: (id) => `/tv/${id}/reviews`,
+    },
+    tvSeason: {
+      url: (id, season_number) => `/tv/${id}/season/${season_number}`,
+    },
+    cast: {
+      url: (id) => `/tv/${id}/credits`
+    }
+  },
+};
+
+export default tv;
