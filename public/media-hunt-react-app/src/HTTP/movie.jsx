a092c97d4c28fddc0cb628426f051fdc49559426
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-11-04 10:25:21 +0100

    http folder, normalization, env implemented in project

---
+++ public/media-hunt-react-app/src/HTTP/movie.jsx
@@ -0,0 +1,65 @@
+import movie from "../API/index"
+import instance from "../client"
+
+async function getMoviesDiscover() {
+  try {
+    const { data } = await instance.get(movie.get.discover.url);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMovieGenres() {
+  try {
+    const { data } = await instance.get(movie.get.genres.url);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMovieTopRated() {
+  try {
+    const { data } = await instance.get(movie.get.topRated.url);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMovie(id) {
+  try {
+    const { data } = await instance.get(movie.get.movie.url(id));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMovieReviews(id) {
+  try {
+    const { data } = await instance.get(movie.get.reviews.url(id));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMovieCast(id) {
+  try {
+    const { data } = await instance.get(movie.get.cast.url(id));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export {
+  getMoviesDiscover,
+  getMovieGenres,
+  getMovieTopRated,
+  getMovie,
+  getMovieReviews,
+  getMovieCast,
+};
