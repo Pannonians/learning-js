1de65c384ec0873eb6dd05f2ff26723476ca03dd
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-11-09 09:24:29 +0100

    required dependencies installed, app ui designed

---
+++ public/media-hunt-react-app/src/scripts/search.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./search.css"
+
+class SearchBox extends Component {
+
+  handleChange(event) {
+    event.target.select();
+  }
+  
+  render() {
+    return (
+      <div className="col-xs-12 search-container nopadding">
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-lg-5">
+          </div>
+          <div className="col-xs-12 col-sm-6 col-lg-7">
+            <form className="searchbox">
+              <input
+                className="searchbox__input typeahead form-control"
+                type="text"
+                placeholder="Search Title..."
+                id="q"
+              />
+              <input type="submit" onClick={this.props.onClick} />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export { SearchBox };
---
+++ public/media-hunt-react-app/src/scripts/searchbutton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MDBBtn } from 'mdbootstrap';
+
+export default function button() {
+  return (
+    <>
+      
+      <MDBBtn outline className='mx-2' color='dark'>
+        Dark
+      </MDBBtn>
+      
+    </>
+  );
+}
