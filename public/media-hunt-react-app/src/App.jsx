0d69048d0ad46c916e7e7ca8f51feed22533d0c7
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-11-09 09:52:31 +0100

    Revert "required dependencies installed, app ui designed"
    This reverts commit 1de65c384ec0873eb6dd05f2ff26723476ca03dd.

--- public/media-hunt-react-app/src/App.jsx
+++ public/media-hunt-react-app/src/App.jsx
@@ -1,26 +1,11 @@
-import "./App.css";
-import { Link } from "react-router-dom";
-import Routes from "./Routes";
+import './App.css';
 
 function App() {
   return (
-    <>
-      <header>
-        <strong>TMDB Movie Database</strong>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {/* {routeResult} */}
-        <Routes />
-      </main>
-    </>
-  )
+    <div className='App'>
+        <div className='flex items-center content-center' style={{fontSize: "18"}}>TMDB React App</div>
+    </div>
+  );
 }
 
 export default App;
