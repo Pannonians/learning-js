90385e44420b8f895207768a2d3230048fd6c6db
Nikolina Novakovic <nikolina.novakovic@pannonians.com>
2022-11-30 14:43:12 +0100

    store timer in storage and keep ticking functionality

--- public/pomodoro-react-app/src/components/timer.jsx
+++ public/pomodoro-react-app/src/components/timer.jsx
@@ -2,41 +2,67 @@ import { useEffect, useState } from "react";
 import Settings from "./settings/settings";
 
 function Timer() {
-  const [time, setTime] = useState(25 * 60) // time in seconds 
-  const [isActive, setIsActive] = useState(false)
-  const [displayMessage, setDisplayMessage] = useState('Start timer')
+  const storageTime = localStorage.getItem("timeLeft");
+  const [time, setTime] = useState(
+    storageTime ? parseInt(storageTime) : 25 * 60
+  );
+  // time in seconds
+  const [isActive, setIsActive] = useState(false);
+  const [displayMessage, setDisplayMessage] = useState("Start timer");
 
   const getTime = () => {
-    const minutes = Math.floor(time / 60)
-    const seconds = time - minutes * 60
-    return `${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`
-  }
+    const timeFromLocalStorage = localStorage.getItem("timeLeft");
+    const minutes = Math.floor(timeFromLocalStorage / 60);
+    const seconds = timeFromLocalStorage - minutes * 60;
+    return `${minutes < 10 ? "0" + minutes : minutes}:${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+  };
 
   const startTimer = () => {
-    setIsActive(!isActive)
-    setDisplayMessage(!isActive ? 'Ticking' : 'Pause')
-  }
+    setIsActive(!isActive);
+    setDisplayMessage(!isActive ? "Ticking" : "Pause");
+  };
 
   const resetTimer = () => {
-    setTime(25 * 60);
-    setIsActive(false)
-    setDisplayMessage(isActive ? 'Ticking' : 'Start timer')
-  }
+    localStorage.setItem("timeLeft", 1500);
+    setTime(localStorage.getItem("timeLeft"));
+    setIsActive(false);
+    setDisplayMessage(isActive ? "Ticking" : "Start timer");
+  };
 
   useEffect(() => {
     if (time > 0 && isActive) {
       const interval = setInterval(() => {
         setTime(time - 1);
-      }, 1000)
-      return () => clearInterval(interval)
+      }, 1000);
+      return () => clearInterval(interval);
     }
-
   }, [time, isActive]);
+
+  useEffect(() => localStorage.setItem("timeLeft", time), [time]);
+
+  window.onload = function () {
+    let timeInterval = 1500;
+    let timeLeft = localStorage.getItem("timeLeft");
+    if (isNaN(timeLeft)) {
+      localStorage.setItem("timeLeft", timeInterval);
+    } else if (timeLeft === 0) {
+      localStorage.setItem("timeLeft", timeInterval);
+    } else {
+      setIsActive(true);
+      timeInterval = parseInt(timeLeft);
+      setTime(timeInterval);
+      localStorage.setItem("timeLeft", time);
+    }
+  };
 
   return (
     <div className="col-sm-12">
       <div className="row">
-        <div className="col-sm-12 left" ><Settings /></div>
+        <div className="col-sm-12 left">
+          <Settings />
+        </div>
       </div>
       <div className="timer timer:hover center">{getTime()}</div>
       <div>
@@ -47,8 +73,18 @@ function Timer() {
         </div>
         <div className="row">
           <div className="col-sm-12 center">
-            <button className="btn button-startstop" onClick={startTimer}>{isActive ? 'Stop' : 'Start'}</button>
-            <button className="btn button-startstop" onClick={resetTimer}>Reset</button>
+            <button
+              className="btn button-startstop"
+              onClick={() => startTimer()}
+            >
+              {isActive ? "Stop" : "Start"}
+            </button>
+            <button
+              className="btn button-startstop"
+              onClick={() => resetTimer()}
+            >
+              Reset
+            </button>
           </div>
         </div>
       </div>
